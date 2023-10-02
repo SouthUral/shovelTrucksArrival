bd7dbbc2ed582abfa8c84d8ebe0020afeea28b6f
vladimir <vladimir1012kov@gmail.com>
2023-10-02 11:36:57 +0300

    #2 добавил go.mod go.sum

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	env "github.com/SouthUral/shovelTrucksArrival/envmanager"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	// логи в формате JSON, по умолчанию формат ASCII
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// логи идут на стандартный вывод, их можно перенаправить в файл
+	log.SetOutput(os.Stdout)
+
+	// установка уровня логирования
+	log.SetLevel(log.InfoLevel)
+}
 
 func main() {
-	fmt.Println("Test")
+	log.Info("Запуск сервиса")
+
+	chEnv := env.InitLoadEnvs()
+	answer := <-chEnv
+	log.Info(answer)
+
 }
