6c26a6449a420fa21a70add5aca1d7f4231e83ab
vladimir <vladimir1012kov@gmail.com>
2023-10-03 18:51:34 +0300

    #3 загрузчик переменных окружения готов

--- envmanager/envmanager.go
+++ envmanager/envmanager.go
@@ -1,7 +1,9 @@
 package envmanager
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -14,32 +16,79 @@ func InitLoadEnvs() AnswerEnvCh {
 	return answerCh
 }
 
-// Функция для загрузки переменной окружения по ключу
-func getEnv(key string) string {
+// Функция для загрузки переменной окружения по ключу.
+// Если переменная не загружена, то функция вернет пустую строку и ошибку.
+func getEnv(key string) (string, error) {
 	value, exists := os.LookupEnv(key)
 	if exists {
-		return value
+		return value, nil
 	}
-	log.Warningf("Переменная %s не загружена", key)
-	return ""
+	err := fmt.Errorf("переменная %s не загружена", key)
+	log.Warning(err.Error())
+	return "", err
 }
 
 // Функция загружает переменные окружения для модулей Postgres и Rabbit.
 // В случае если какие-то переменные не загружены, будет сформирована ошибка
 func loadEnvs(answerCh AnswerEnvCh) {
+	var err interface{}
 
-}
+	pgEnv, errPg := loadPostgresEnv()
+	rbEnv, errRb := loadRabbitEnv()
 
-func loadingVariablesIntoStruct[filledStruct EnvPostgres | EnvRabbitMQ](envs envAlias) filledStruct {
-	for key, value := range envs {
-
+	if errPg != nil || errRb != nil {
+		err = fmt.Errorf("не загружены env %s, %s", errPg, errRb)
+	} else {
+		err = nil
 	}
+
+	responce := AnswerEnv{
+		PostgresURL = pgEnv.getPostgresURL(),
+		RabbitURL = rbEnv.getRabbitURL(),
+		Error = err,
+	}
+	answerCh <- responce
 }
 
-func loadPostgresEnv() {
+func loadingEnvVar(fieldsAndAliasEnv envAlias) (envStorageStruct, error) {
+	envStorage := envStorageStruct{}
+	errorFields := make([]string, 0)
 
+	for fieldName, envAlias := range fieldsAndAliasEnv {
+		loadedEnv, err := getEnv(envAlias)
+		if err != nil {
+			errorFields = append(errorFields, envAlias)
+		}
+		envStorage.readAndWriteField(fieldName, loadedEnv)
+	}
+
+	if len(errorFields) > 0 {
+		errRes := fmt.Errorf("%s", strings.Join(errorFields, ", "))
+		return envStorage, errRes
+	}
+	return envStorage, nil
 }
 
-func loadRabbitEnv() {
+func loadPostgresEnv() (envStorageStruct, error) {
+	envsAlias := envAlias{
+		"Host":     "ASD_POSTGRES_HOST",
+		"Port":     "ASD_POSTGRES_PORT",
+		"Login":    "ASD_POSTGRES_LOGIN",
+		"Password": "ASD_POSTGRES_PASSWORD",
+		"DBName":   "ASD_POSTGRES_DBNAME",
+	}
+	loadedEnvs, err := loadingEnvVar(envsAlias)
+	return loadedEnvs, err
+}
 
+func loadRabbitEnv() (envStorageStruct, error) {
+	envsAlias := envAlias{
+		"Host":     "ASD_RMQ_HOST",
+		"Port":     "ASD_RMQ_PORT",
+		"Login":    "ASD_RMQ_LOGIN",
+		"Password": "ASD_RMQ_PASSWORD",
+		"VHost":    "ASD_RMQ_HOST",
+	}
+	loadedEnvs, err := loadingEnvVar(envsAlias)
+	return loadedEnvs, err
 }
--- envmanager/models.go
+++ envmanager/models.go
@@ -1,25 +1,56 @@
 package envmanager
 
+import "fmt"
+
 // Канал для отправки сообщения AnswerEnv от LoadEnvs
 type AnswerEnvCh chan AnswerEnv
 
 // Сообщение, которое вернется из горутины LoadEnvs
-type AnswerEnv interface{}
+type AnswerEnv struct {
+	PostgresURL string
+	RabbitURL   string
+	Error       error
+}
 
 // Структура для хранения переменных окружения, необходимых для подключения к БД Postgres
-type EnvPostgres struct {
-	Host     string
-	Port     string
-	Login    string
-	Password string
-	DBName   string
+type envStorageStruct struct {
+	Host      string
+	Port      string
+	Login     string
+	Password  string
+	LastField string
 }
 
-// Структура для хранения переменных окружения, необходимых для подключения к RabbitMQ
-type EnvRabbitMQ struct {
-	Host     string
-	Port     string
-	Login    string
-	Password string
-	VHost    string
+// Метод формирует корневой элемент URL для подключения
+func (envStorage *envStorageStruct) formationConnString() string {
+	return fmt.Sprintf("%s:%s@%s:%s/%s", envStorage.Login, envStorage.Password, envStorage.Host, envStorage.Port, envStorage.LastField)
 }
+
+// Метод для формирования URL для подключения к Postgres DB
+func (envStorage *envStorageStruct) getPostgresURL() string {
+	return fmt.Sprintf("postgresql://%s", envStorage.formationConnString())
+}
+
+// Метод для формирования URL для подключения к RabbitMQ
+func (envStorage *envStorageStruct) getRabbitURL() string {
+	return fmt.Sprintf("amqp://%s", envStorage.formationConnString())
+}
+
+// Метод для записи переменных окружения в структуру по наименованию поля
+func (envStorage *envStorageStruct) readAndWriteField(NameFiled, ValueField string) {
+	switch NameFiled {
+	case "Host":
+		envStorage.Host = ValueField
+	case "Port":
+		envStorage.Port = ValueField
+	case "Login":
+		envStorage.Login = ValueField
+	case "Password":
+		envStorage.Password = ValueField
+	case "DBName", "VHost":
+		envStorage.LastField = ValueField
+	}
+}
+
+// Словарь, который должен содержать имена переменных окружения
+type envAlias map[string]string
